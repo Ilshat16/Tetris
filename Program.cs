d61c4dd102529e5b1446b8677fe26dc0c6758fd6
Ilshat <illliat@mail.ru>
2023-01-27 14:22:20 +0300

    Add action menu

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@
     }
 }
 
+void ShowMenu()
+{
+    System.Console.WriteLine("Меню выбора действия:");
+    System.Console.WriteLine("1 - переместить влево");
+    System.Console.WriteLine("2 - переместить вправо");
+    System.Console.WriteLine("3 - повернуть");
+    System.Console.WriteLine("4 - закончить ход");
+}
+
 void CreateBorder(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(1); i++)
@@ -21,6 +30,7 @@ void CreateBorder(int[,] matrix)
 
 void PrintMatrix(int[,] matrix)
 {
+    Console.Clear();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
@@ -37,3 +47,6 @@ int[,] tetrisBoard = new int[border, border];
 CreateBorder(tetrisBoard);
 CreateLine(tetrisBoard);
 PrintMatrix(tetrisBoard);
+ShowMenu();
+System.Console.Write("Выберите нужный пункт: ");
+int actionNum = int.Parse(Console.ReadLine());
