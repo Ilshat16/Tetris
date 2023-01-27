a85f9fbed7158eeaff63b0cf384812d4e63d4548
Ilshat <illliat@mail.ru>
2023-01-27 14:08:07 +0300

    Create border and first figure (line)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿
+﻿void CreateLine(int[,] matrix)
+{
+    int lineLength = 4;
+    int middle = matrix.GetLength(1) / 2;
+    for (int i = 0; i < lineLength; i++)
+    {
+        matrix[1, (middle - lineLength / 2) + i] = 1;
+    }
+}
+
+void CreateBorder(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        matrix[0, i] = 1;
+        matrix[i, 0] = 1;
+        matrix[i, matrix.GetLength(1) - 1] = 1;
+        matrix[matrix.GetLength(1) - 1, i] = 1;
+    }
+}
 
 void PrintMatrix(int[,] matrix)
 {
@@ -6,13 +25,15 @@ void PrintMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (matrix[i, j] == 0) System.Console.Write(" ");
+            if (matrix[i, j] == 0) System.Console.Write("  ");
             else System.Console.Write("+ ");
         }
         System.Console.WriteLine();
     }
 }
 
-int border = 3;
+int border = 20;
 int[,] tetrisBoard = new int[border, border];
+CreateBorder(tetrisBoard);
+CreateLine(tetrisBoard);
 PrintMatrix(tetrisBoard);
