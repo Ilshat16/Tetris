b0ff7d9aae9edc587887314fa75752ee6950f9ce
Ilshat <illliat@mail.ru>
2023-01-27 13:54:20 +0300

    Add matrix print function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == 0) System.Console.Write(" ");
+            else System.Console.Write("+ ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int border = 3;
+int[,] tetrisBoard = new int[border, border];
+PrintMatrix(tetrisBoard);
